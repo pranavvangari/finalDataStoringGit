06b536b0ecbdf4ccb87c5f89ba3ef54d9b37c013
Pranav <pranavvvangari@gmail.com>
2022-12-08 09:39:27 -0600

    outputting data from database works
    Signed-off-by: Pranav <pranavvvangari@gmail.com>

--- dataStoringMP/Storing Data - Mini Project/Website/models.py
+++ dataStoringMP/Storing Data - Mini Project/Website/models.py
@@ -3,12 +3,23 @@ from . import db
 class Name(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(100))
-    temp_id = db.Column(db.Integer, db.ForeignKey('temp.id'))
+    temp_id = db.Column(db.Integer)
+    
+
+def displayNames(currentArr):
+    namesModel = []
+    for x in currentArr:
+        namesModel.append(currentArr[x])
+    
+    return namesModel
 
 
-class Temp(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    allNames = db.relationship('Name')   
+    
+    
+
+
+
+   
    
 
 
--- dataStoringMP/Storing Data - Mini Project/Website/views.py
+++ dataStoringMP/Storing Data - Mini Project/Website/views.py
@@ -1,27 +1,38 @@
 from flask import Blueprint, flash, render_template, request
-from .models import Name, Temp
+from .models import Name, displayNames
 from . import db
 
 views = Blueprint('views', __name__)
 
+namesArr = ["josh", "pranav"]
+#namesArr.append(Name.query.filter_by(data=Name.data).all())
+
 @views.route('/', methods=['GET', 'POST'])
 def home():
-    name = request.form.get('name') 
+    name = request.form.get('name')
+    
+    
+
     if request.method == 'POST':
-        #name = request.form.get('name') 
+        
+        print(namesArr)
+        
         if len(name) > 1:
             new_name = Name(data=name)
+            
             db.session.add(new_name)
             db.session.commit() 
+            #namesArr.append(new_name.data)
+            #displayNames(namesArr)
+            print("namesArr")
             flash('Saved', category='success')
 
         else:
 
             flash('Not saved', category='error')
+    
 
-
-
-    return render_template("home.html", namesToDisplay=Temp()) 
+    return render_template("home.html", namesToDisplay=Name.query.all()) 
 
 
 
